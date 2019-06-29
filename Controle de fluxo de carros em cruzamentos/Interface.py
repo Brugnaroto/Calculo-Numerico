75e2e23cd65739a3dab452a598267641cf5f3013
Mateus Brugnaroto <brugnaroto@gmail.com>
2019-06-29 00:15:54 -0300

    Final layout

--- Controle de fluxo de carros em cruzamentos/Interface.py
+++ Controle de fluxo de carros em cruzamentos/Interface.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from pygame.locals import *
+from datetime import datetime
 
 def car_position(x,y,carImg):
     screen.blit(carImg, (x,y))
@@ -73,7 +75,7 @@ x8 = (display_width*0.625)
 y8 = (display_height*0.01)
 car8 = car_size(50,80,car8Img)
 car8 = pygame.transform.rotate(car8,180)
-
+aux = x5
 R = 0
 S = 0
 T = 0
@@ -82,7 +84,10 @@ V = 0
 X = 0
 W = 0
 Z = 0
+
 troca = 0
+troca2 = 0
+troca3 = 0
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -98,33 +103,35 @@ while True:
     
     car_position(x,y, asfalto)
 
-    if x1+79 == x7 or x3-61 == x6:
+    #Cruzamento A
+    if x1+79 == x7 or x3-61 == x6 or y5-79 == y1:
         R+=1   
-    if y7+79 == y3 or y6-61 == y1:
+    if y7-5 == y3:
         S+=1
-
+    #Cruzamento B
     if y8+79 == y3 or y5-61 == y1:
         T+=1
     if x1+81 == x8 or x3-59 == x6:
         U+=1
-
-    #Erro em V e W
-    #print(x2, x8)
+    #Cruzamento C
     if x2+30 == x7 or x4-61 == x6:
         V+=1
     if y7+70 == y3 or y6-70 == y1:
         X+=1
-
-    if x2+70 == x8 or x4-361 == x5:
+    #Cruzamento D
+    if x4-80 == x8 or x2+70 == x8:
         Z+=1
     if y8+81 == y4 or y5-30 == y2:
         W+=1
-        
+    if y1+81 == y4:
+        W+=1
+
+    #Colisões   
     if x1+80 == x7:
         x1 = x1
-        if (y7 > 0 and y7 < 200) or y7 > 550:
+        if (y7 > 0 and y7 < 200):
             x1 = x1
-        elif (y5 > 200 and y5 < 300) or (y5 > 100 and y5 < 450):
+        elif (y5 > 100 and y5 < 450):
             x1 = x1
         else:
             x1+=1
@@ -134,11 +141,15 @@ while True:
 
         if (y8 > 0 and y8 < 200) or y8 > 550:
             x1 = x1
-        elif (y6 > 200 and y6 < 300) or (y6 > 100 and y6 < 450):
-            x1 = x1
         else:
             x1+=1
-
+ 
+    #elif x1 == -70:
+     #   x1 = x1
+      #  if (wait > 60):
+       #     wait = 2
+       # if (datetime.now().second == wait):
+        #    x1+=1
     else:
         x1+=1
         
@@ -151,59 +162,63 @@ while True:
             x2 = x2
         else:
             x2+=2
-            
+
     elif x2+80 == x8:
         x2 = x2
-
+        
         if y8 < 450 and y8 > 200:
             x2 = x2
         elif y6 > 300 and y6 > 200:
             x2 = x2
+        elif y1 < 450 and y1 > 200:
+            x2 = x2
         else:
             x2+=2
-
     else:
         x2+=2
-    
-    if x3-60 == x6:
+
+    if x3-80 == x6:
         x3 = x3
-        
-        if y6 < 300 and y6 > 50:
+
+        if (y6 > 0 and y6 < 300):
             x3 = x3
-        elif y8 < 100 or y8 > 450:
+        elif y8 > 550:
+            x3 = x3
+        elif y8 >= -60 and y8 < 100:
             x3 = x3
         else:
             x3-=1
             
-    elif x3-60 == x5:
+    elif x3-80 == x5:
         x3 = x3
-
-        if y5 < 300 and y5 > 50:
+        
+        if y5 > 0 and y5 < 300:
             x3 = x3
-        elif y7 < 100 or y7 > 450:
+        elif y7 > 500:
+            x3 = x3
+        elif y7 >= -80 and y7 < 300:
             x3 = x3
         else:
             x3-=1
-
     else:
-        x3-=1      
+        x3-=1
         
     if x4-60 == x6:
         x4 = x4
         
-        if y6 < 600 and y6 > 250:
+        if y6 < 550 and y6 > 250:
             x4 = x4
         elif y8 < 400 and y8 > 150:
+            x4 = x4
+        elif y1 < 400 and y1 > 200:
             x4 = x4
         else:
             x4-=1
             
-    elif x4-60 == x5:
+    elif x4-60 == aux:
         x4 = x4
 
-        if y5 < 600 and y5 > 250:
-            x4 = x4
-        elif y7 < 400 and y7 > 150:
+        if y5 < 600 and y5 > 250 or (y7 > 100 and y7 < 400):
             x4 = x4
         else:
             x4-=1
@@ -211,37 +226,76 @@ while True:
     else:
         x4-=1
 
+    if y7+80 == y3:
+        y7 = y7
+  
+        if x5 > x7-80 and y5 < 400:
+            y7 = y7
+        elif x3 >= x7-60 and x3 < x5+200:
+            y7 = y7
+        else:
+            y7+=1
+    else:
+        y7+=1
     y5-=1
     y6-=1
-    y7+=1
     y8+=1
 
-    if x1 == display_width:
-        x1 = -60
-    if x2 == display_width:
-        x2 = -60
+        #step = (random.randint(0, 10))
+        #time = (datetime.now().second)
+        #wait = step + time
+    if x2 == 860:
+        x2 = -70
     if x3 == -60:
         x3 = 860
     if x4 == -60:
         x4 = 860
-    if y5 == -60:
-        y5 = 650
     if y6 == -60:
         y6 = 650
     if y7 == display_height:
         y7 = -50
     if y8 == display_height:
-        y8 = -50
+        y8 = -60
+    if y1 == display_height:
+        x1 = -70
+        y1 = (display_height*0.26)
+        troca = 0
+        car1 = pygame.transform.rotate(car1,90)
+    if x5 == -50:
+        x5 = (display_width*0.315)
+        y5 = 650
+        troca3 = 0
+        car5 = pygame.transform.rotate(car5,270)
     
-    car_position(x1, y1, car1)
+    if x1 == x8:
+        if (troca == 0):
+            car1 = pygame.transform.rotate(car1,270)
+        troca = 1
+    if troca == 1:
+        x1 = x8
+        y1 = y1 + 1
+        car_position(x1,y1,car1)
+    if troca == 0:
+        car_position(x1, y1, car1)
+
+    if y5 == y3:
+        if (troca3 == 0):
+            car5 = pygame.transform.rotate(car5,90)
+        troca3 = 1
+    if troca3 == 1:
+        y5 = y3
+        x5 = x5 - 1
+        car_position(x5,y5,car5)
+    if troca3 == 0:
+        car_position(x5, y5, car5)
+
     car_position(x2, y2, car2)
     car_position(x3, y3, car3)
     car_position(x4, y4, car4)
-    car_position(x5, y5, car5)
     car_position(x6, y6, car6)
     car_position(x7, y7, car7)
     car_position(x8, y8, car8)
     
-    #pygame.display.flip()       
+    pygame.display.flip()       
     pygame.display.update()
     
